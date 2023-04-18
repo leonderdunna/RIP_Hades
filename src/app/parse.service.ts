b90cea5ead645b3f16996383ede670abeb95e4ec
Leonard Menzel <leonderdunna@gmail.com>
2023-04-18 10:44:51 +0200

    bisschen dies das in parse service

--- src/app/parse.service.ts
+++ src/app/parse.service.ts
@@ -5,15 +5,23 @@ import {Injectable} from '@angular/core';
 })
 export class ParseService {
 
+  last_parsed: any;
+
+  getCurrent(): any {
+    return this.last_parsed;
+  }
 
   parseHadesFile(f: string): any {
+
     console.log(f)
 
-    return {
+    let o: any = {
       components: this.parseComponents(this.extractBlock(f, "components")),
       signals: this.extractBlock(f, "signals"),
       name: this.extractName(f)
     }
+    this.last_parsed = 0;
+    return o
 
   }
 
@@ -41,10 +49,12 @@ export class ParseService {
         return {
           typ: e[0],
           coords: {
-            //TODO vielleicht auch andersrum
-            x:e[2],
-            y:e[3]
-          }
+            x: e[2],
+            y: e[3]
+          },
+          name: e[1],
+          // E[6] ChatGPT behauptet, es wäre der delay der komponente. ergibt sinn für mich
+          delay: e[6]
         }
       })
   }

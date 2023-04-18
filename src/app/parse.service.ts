819c555fcc6238511897d637191d8b0670bc9348
Leonard Menzel <leonderdunna@gmail.com>
2023-04-18 10:27:13 +0200

    anfang parsecomponent

--- src/app/parse.service.ts
+++ src/app/parse.service.ts
@@ -10,27 +10,43 @@ export class ParseService {
     console.log(f)
 
     return {
-      components:this.extractBlock(f,"components"),
-      signals:this.extractBlock(f,"signals"),
+      components: this.parseComponents(this.extractBlock(f, "components")),
+      signals: this.extractBlock(f, "signals"),
       name: this.extractName(f)
     }
 
   }
 
-  extractBlock(f:string,s:string):string[]{
+  extractBlock(f: string, s: string): string[] {
     // @ts-ignore
     return f
-      .match(new RegExp(`\\[${s}](.*)\\[end ${s}]`,"gs"))[0]
-      .replaceAll(/(\[[^\[\]}]*])/gs,"")
+      .match(new RegExp(`\\[${s}](.*)\\[end ${s}]`, "gs"))[0]
+      .replaceAll(/(\[[^\[\]}]*])/gs, "")
       .trim()
       .split(/[\n\r]+/)
       .filter(e => e !== "")
 
   }
-  extractName(f:string):string{
+
+  extractName(f: string): string {
     let l = f.trim().split(/[\n\r]+/)
     return l[2].split(/\s+/)[1]
 
+  }
+
+  parseComponents(lines: string[]): any {
+    return lines
+      .map(e => e.trim().split(/\s+/))
+      .map(e => {
+        return {
+          typ: e[0],
+          coords: {
+            //TODO vielleicht auch andersrum
+            x:e[2],
+            y:e[3]
+          }
+        }
+      })
   }
 
 }
